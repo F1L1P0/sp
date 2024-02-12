0c9544fb31da30779fa1692af659ba9f2d6282d1
Filip Drábek <drabekfilip@seznam.cz>
2024-02-12 10:31:55 +0100

    idk anymore why safari laggy :(

--- script.js
+++ script.js
@@ -1,53 +1,47 @@
-function openModal() {
-  document.getElementById('myModal').style.display = 'block'
-}
+const modal = document.getElementById('myModal')
+const slides = document.getElementsByClassName('mySlides')
+const dots = document.getElementsByClassName('demo')
+const navDropdown = document.getElementById('navDropdown')
+const navBtn = document.getElementById('navBtn')
 
-// Close the Modal
-function closeModal() {
-  document.getElementById('myModal').style.display = 'none'
-}
-
-var slideIndex = 1
+let slideIndex = 1
 showSlides(slideIndex)
 
-// Next/previous controls
+function openModal() {
+  modal.style.display = 'block'
+}
+
+function closeModal() {
+  modal.style.display = 'none'
+}
+
 function plusSlides(n) {
   showSlides((slideIndex += n))
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n))
 }
 
 function showSlides(n) {
-  var i
-  var slides = document.getElementsByClassName('mySlides')
-  var dots = document.getElementsByClassName('demo')
-  //var captionText = document.getElementById('caption')
   if (n > slides.length) {
     slideIndex = 1
   }
   if (n < 1) {
     slideIndex = slides.length
   }
-  for (i = 0; i < slides.length; i++) {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = 'none'
   }
-  for (i = 0; i < dots.length; i++) {
+  for (let i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(' active', '')
   }
   slides[slideIndex - 1].style.display = 'block'
   dots[slideIndex - 1].className += ' active'
-  //captionText.innerHTML = dots[slideIndex - 1].alt
 }
 
-const navDropdown = document.querySelector('#navDropdown')
-const navBtn = document.querySelector('#navBtn')
-
 document.addEventListener('click', function (e) {
-  const target = e.target
-  if (target === navDropdown || target.closest('#navBtn')) {
+  if (e.target === navDropdown || e.target.closest('#navBtn')) {
     navDropdown.classList.toggle('toggleNav')
   } else return
 })
