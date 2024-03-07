29139f1c7cfde1005067cd377496c50b0f650dc6
Filip Drábek <drabekfilip@seznam.cz>
2024-03-07 22:03:19 +0100

    counting number effect  image changes

--- script.js
+++ script.js
@@ -84,3 +84,53 @@ function updateParallax() {
 window.addEventListener('scroll', () => {
   requestAnimationFrame(updateParallax)
 })
+
+function animateValue(id, start, end, duration) {
+  var range = end - start
+  var current = start
+  var obj = document.getElementById(id)
+  var starttime
+  var fn = (ms) => {
+    let progress = 0
+    if (starttime === undefined) {
+      starttime = ms
+    } else {
+      progress = ms - starttime
+      if (progress >= duration) {
+        current = end.toLocaleString()
+      } else {
+        current = start + Math.floor((progress / duration) * range)
+      }
+    }
+    obj.innerHTML = current.toLocaleString() + '+'
+    if (progress < duration) {
+      requestAnimationFrame(fn)
+    }
+  }
+  requestAnimationFrame(fn)
+}
+
+// Check if the element is in the viewport
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect()
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
+}
+
+// Function to handle scroll event
+function handleScroll() {
+  var element = document.getElementById('value')
+  if (isInViewport(element)) {
+    animateValue('value', 0, 45000, 1560)
+    animateValue('years', 0, 9, 1000)
+    window.removeEventListener('scroll', handleScroll)
+  }
+}
+
+// Add event listener for scroll event
+window.addEventListener('scroll', handleScroll)
